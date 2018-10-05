ee849408c60ceda70d282e2ca425686af492fb03
Fanda <fanda51@centrum.cz>
2018-10-05 11:01:11 +0200

    Initial Commit

---
+++ PA1 proseminar/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PA1 proseminar
+//
+//  Created by Frantisek Stepanek on 05/10/2018.
+//  Copyright © 2018 Frantisek Stepanek. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
