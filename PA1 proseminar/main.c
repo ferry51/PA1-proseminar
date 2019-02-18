07770f935c16258a8d38822717c844ba3ffdb6dc
Fanda <fanda51@centrum.cz>
2019-02-18 14:46:03 +0100

    a

--- PA1 proseminar/main.c
+++ PA1 proseminar/main.c
@@ -6,10 +6,10 @@
 //  Copyright © 2018 Frantisek Stepanek. All rights reserved.
 //
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include <limits.h>
+int main(){
+#define MIN(X,Y) ((X) < (Y) ? X : Y)
+    int  a=6, b=5;
+    printf("%d %d %d ",MIN(a++,b++),a,b);
     return 0;
 }
